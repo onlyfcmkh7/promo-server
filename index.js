d478b77dde9cced49f18ee04ac30fef3fa9ff8a7
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-10 12:47:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const { scrapeATB } = require("./parsers/atb");
 const { scrapeSilpo } = require("./parsers/silpo");
+const { scrapeMetro } = require("./parsers/metro");
 
 const app = express();
 app.use(cors());
@@ -27,6 +28,17 @@ app.get("/promotions/silpo", async (_req, res) => {
     res.json(data);
   } catch (e) {
     console.error(e);
+    res.status(500).json({ error: "fail" });
+  }
+});
+
+// 🟡 METRO
+app.get("/promotions/metro", async (_req, res) => {
+  try {
+    const data = await scrapeMetro();
+    res.json(data);
+  } catch (e) {
+    console.error("METRO ERROR:", e);
     res.status(500).json({ error: "fail" });
   }
 });
